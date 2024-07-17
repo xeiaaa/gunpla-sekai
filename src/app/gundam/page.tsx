22b0b4ac69ab0d758e93c14fa35be93f90a759b4
Bret Axl Sebastian Pura <cosmicdevcs@gmail.com>
2024-07-18 02:16:33 +0800

    home page setup

--- src/app/gundam/page.tsx
+++ src/app/gundam/page.tsx
@@ -15,76 +15,77 @@ import { useState } from "react";
 export default function GundamPage() {
   const [selectedSeries, setSelectedSeries] = useState(series[0]);
 
-  return (
-    <Flex direction="row" flex={1} gap={8}>
-      <Box pos="relative" w={200}>
-        <ScrollArea
-          pos="absolute"
-          top={0}
-          bottom={0}
-          right={0}
-          left={0}
-          type="never">
-          {series
-            .filter((media) => !!media.logoSrc)
-            .map((media) => {
-              return (
-                <UnstyledButton
-                  key={media.id}
-                  onClick={() => setSelectedSeries(media)}>
-                  <Image
-                    src={media.logoSrc as string}
-                    alt="Mobile Suit Gundam Logo"
-                    width={200}
-                    height={120}
-                    style={{ objectFit: "contain" }}
-                  />
-                </UnstyledButton>
-              );
-            })}
-        </ScrollArea>
-      </Box>
-      <SimpleGrid
-        cols={{ base: 1, sm: 2, md: 3, lg: 4 }}
-        spacing={{ base: 2, sm: "xl" }}
-        verticalSpacing={{ base: "md", sm: "xl" }}>
-        {Array.isArray(selectedSeries.mobileSuits) &&
-          selectedSeries.mobileSuits.map((ms) => {
-            return (
-              <Flex direction="column" key={ms.id}>
-                <img src={ms.imageSrc} alt={ms.name} />
-                <Text>{ms.name}</Text>
-              </Flex>
-            );
-          })}{" "}
-        {Array.isArray(selectedSeries.mobileSuits) &&
-          selectedSeries.mobileSuits.map((ms) => {
-            return (
-              <Flex direction="column" key={ms.id}>
-                <img src={ms.imageSrc} alt={ms.name} />
-                <Text>{ms.name}</Text>
-              </Flex>
-            );
-          })}{" "}
-        {Array.isArray(selectedSeries.mobileSuits) &&
-          selectedSeries.mobileSuits.map((ms) => {
-            return (
-              <Flex direction="column" key={ms.id}>
-                <img src={ms.imageSrc} alt={ms.name} />
-                <Text>{ms.name}</Text>
-              </Flex>
-            );
-          })}{" "}
-        {Array.isArray(selectedSeries.mobileSuits) &&
-          selectedSeries.mobileSuits.map((ms) => {
-            return (
-              <Flex direction="column" key={ms.id}>
-                <img src={ms.imageSrc} alt={ms.name} />
-                <Text>{ms.name}</Text>
-              </Flex>
-            );
-          })}
-      </SimpleGrid>
-    </Flex>
-  );
+  return null;
+  // return (
+  //   <Flex direction="row" flex={1} gap={8}>
+  //     <Box pos="relative" w={200}>
+  //       <ScrollArea
+  //         pos="absolute"
+  //         top={0}
+  //         bottom={0}
+  //         right={0}
+  //         left={0}
+  //         type="never">
+  //         {series
+  //           .filter((media) => !!media.logoSrc)
+  //           .map((media) => {
+  //             return (
+  //               <UnstyledButton
+  //                 key={media.id}
+  //                 onClick={() => setSelectedSeries(media)}>
+  //                 <Image
+  //                   src={media.logoSrc as string}
+  //                   alt="Mobile Suit Gundam Logo"
+  //                   width={200}
+  //                   height={120}
+  //                   style={{ objectFit: "contain" }}
+  //                 />
+  //               </UnstyledButton>
+  //             );
+  //           })}
+  //       </ScrollArea>
+  //     </Box>
+  //     <SimpleGrid
+  //       cols={{ base: 1, sm: 2, md: 3, lg: 4 }}
+  //       spacing={{ base: 2, sm: "xl" }}
+  //       verticalSpacing={{ base: "md", sm: "xl" }}>
+  //       {Array.isArray(selectedSeries.mobileSuits) &&
+  //         selectedSeries.mobileSuits.map((ms) => {
+  //           return (
+  //             <Flex direction="column" key={ms.id}>
+  //               <img src={ms.imageSrc} alt={ms.name} />
+  //               <Text>{ms.name}</Text>
+  //             </Flex>
+  //           );
+  //         })}{" "}
+  //       {Array.isArray(selectedSeries.mobileSuits) &&
+  //         selectedSeries.mobileSuits.map((ms) => {
+  //           return (
+  //             <Flex direction="column" key={ms.id}>
+  //               <img src={ms.imageSrc} alt={ms.name} />
+  //               <Text>{ms.name}</Text>
+  //             </Flex>
+  //           );
+  //         })}{" "}
+  //       {Array.isArray(selectedSeries.mobileSuits) &&
+  //         selectedSeries.mobileSuits.map((ms) => {
+  //           return (
+  //             <Flex direction="column" key={ms.id}>
+  //               <img src={ms.imageSrc} alt={ms.name} />
+  //               <Text>{ms.name}</Text>
+  //             </Flex>
+  //           );
+  //         })}{" "}
+  //       {Array.isArray(selectedSeries.mobileSuits) &&
+  //         selectedSeries.mobileSuits.map((ms) => {
+  //           return (
+  //             <Flex direction="column" key={ms.id}>
+  //               <img src={ms.imageSrc} alt={ms.name} />
+  //               <Text>{ms.name}</Text>
+  //             </Flex>
+  //           );
+  //         })}
+  //     </SimpleGrid>
+  //   </Flex>
+  // );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,46 @@
-import { Title, Text } from "@mantine/core";
+"use client";
+import { Title, Text, Container, Button, Flex } from "@mantine/core";
+import classes from "./page.module.css";
 
 export default function HomePage() {
   return (
-    <div>
-      <Title order={1}>Home</Title>
+    <div className={classes.root}>
+      <Container size="lg" pos="relative">
+        <Flex>
+          <div className={classes.content}>
+            <Title className={classes.title}>
+              Dedicated to
+              <br />
+              <Text
+                component="span"
+                inherit
+                variant="gradient"
+                gradient={{ from: "pink", to: "yellow" }}>
+                Gunpla (ガンプラ)
+              </Text>{" "}
+              and anything model kit related
+            </Title>
+
+            <Text className={classes.description} mt={30}>
+              {`At Gunpla Sekai, we're dedicated to providing the ultimate Gunpla
+              experience. Explore our comprehensive shop for the latest kits and
+              tools, customize your builds with our advanced 3D modeling
+              services, and share your creations with a community of passionate
+              builders. Whether you're shopping, creating, or showcasing, Gunpla
+              Sekai is your go-to destination for all things Gunpla.`}
+            </Text>
+
+            <Button
+              variant="gradient"
+              gradient={{ from: "pink", to: "yellow" }}
+              size="xl"
+              className={classes.control}
+              mt={40}>
+              Get started
+            </Button>
+          </div>
+        </Flex>
+      </Container>
     </div>
   );
 }
